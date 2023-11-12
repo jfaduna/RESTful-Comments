fbb44e1bb31875e47fec3b426187a8a966b43ca8
jfaduna <flomeraduna@gmail.com>
2023-11-12 15:44:58 +1100

    add: initial commit. Added get list of comments

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8trBoi',
+        comment: 'Please delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    },
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
+app.listen(3000, () => {
+    console.log('LISTENING ON PORT 3000')
+})
